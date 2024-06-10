ce86a70ddaaa88ea0d802c81037eaa148005d968
prooflesben <prooflesben@gmail.com>
2024-06-09 23:38:16 -0400

    Changes

--- frontend3/sca-frontend/src/index.js
+++ frontend3/sca-frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './frontend2/App';
-import reportWebVitals from './frontend2/reportWebVitals';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
